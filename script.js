93e537035795aec310fc59943f69eb623e58b655
mikael <mikael.fangel@pm.me>
2021-06-26 01:12:02 +0200

    made text area selectable again

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function selectTxtArr() {
+	var txtarea = document.getElementById("editor");
+
+	txtarea.focus();
+	txtarea.setFocusAt(0);
+}
+
 function fetch() {
 	console.log(document.getElementById("editor").value);
 }
