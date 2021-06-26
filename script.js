40819844a4bf5d04627bddf1d24bef142cfa8137
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2021-06-26 11:00:15 +0200

    added simple localstorage

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.onload = function storageServ() {
+	var txtarea = document.getElementById("editor");
+
+	localStorage = window.localStorage
+	txtarea.value = localStorage.getItem('savedTxt')
+	fetch();
+}
+
 function selectTxtArr() {
 	var txtarea = document.getElementById("editor");
 	
@@ -7,8 +15,10 @@ function selectTxtArr() {
 function fetch() {
 	var txtarea = document.getElementById("editor");
 	var hiddenEd = document.getElementById("hiddenEditor");
+	var localStorage = window.localStorage
 	
 	var text = txtarea.value;
+	localStorage.setItem('savedTxt', text)
 	text = text.replace(/\r?\n/g,'<br>');	// replaces linebreaks with html tags in the hidden div
 	hiddenEd.innerHTML = text;		// breaks code when writing html
 }
